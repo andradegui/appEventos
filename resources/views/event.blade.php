0d48a53819997017586b1e767b866844548d7210
Guilherme Lima <guilima102013@gmail.com>
2024-03-10 20:27:10 -0300

    Criando configuração de exibir fotos dos eventos

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -11,10 +11,70 @@
     <div class="row">
 
         <div class="col-12">
-            Evento :  {{ $event->title }}
+
+            <h2>Evento :  {{ $event->title }}</h2>
+
+            <p> Evento acontecerá em: {{ $event->start_event->format('d/m/Y H:i:s') }} </p>
+
         </div>
 
     </div>
 
+    <div class="row">
+
+        <div class="col-12">            
+
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+
+                <li class="nav-item" role="presentation">
+
+                  <a class="nav-link active text-dark" id="about-tab" data-toggle="tab" href="#about" role="tab" aria-controls="about" aria-selected="true">Sobre</a>
+
+                </li>
+
+                @if( $event->photos->count() )
+                    <li class="nav-item" role="presentation">
+                        
+                    <a class="nav-link text-dark" id="photo-tab" data-toggle="tab" href="#photo" role="tab" aria-controls="photo" aria-selected="false">Fotos</a>
+
+                    </li>
+                @endif
+
+                </ul>
+
+              <div class="tab-content" id="myTabContent">
+
+                <div class="tab-pane fade show active pt-5" id="about" role="tabpanel" aria-labelledby="about-tab">
+
+                    {{ $event->body }}
+
+                </div>
+
+                @if( $event->photos->count() )
+                    <div class="tab-pane fade pt-5" id="photo" role="tabpanel" aria-labelledby="photo-tab">
+
+                        <div class="row">
+
+                            @foreach($event->photos as $photo) 
+
+                                <div class="col-3">
+
+                                    <img src="{{ $photo->photo }}" alt="Foto do evento {{ $event->title }}" class="img-fluid">
+
+                                </div>
+
+                            @endforeach 
+
+                        </div>                    
+
+                    </div>
+                @endif
+
+              </div>
+
+        </div>
+
+
+    </div>
 
 @endsection
