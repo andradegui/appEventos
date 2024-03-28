33ad71a1978a32f6f29f6140324667b534debfd8
Guilherme Lima <guilima102013@gmail.com>
2024-03-28 16:04:28 -0300

    Chamando rota pelo apelido na tela de detalhe evento, Adicionando espaçamento no topo da página

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -8,15 +8,17 @@
 
 @section('content')
 
-    <div class="row">
-
-        <div class="col-12">
-
+    <div class="row justify-content-between align-items-center">
+        
+        <div class="col-10 my-5">
+            
             <h2>Evento :  {{ $event->title }}</h2>
-
+            
             <p> Evento acontecerá em: {{ $event->start_event->format('d/m/Y H:i:s') }} </p>
-
+            
         </div>
+
+        <a href="{{route('index')}}" class="btn btn-dark">Voltar</a>        
 
     </div>
 
