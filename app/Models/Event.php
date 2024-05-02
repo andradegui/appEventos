bd80a9cb2feaeb3779bf3455d391a07fe2e53518
Guilherme Lima <guilima102013@gmail.com>
2024-05-02 10:39:46 -0300

    Criando mutator p/ salvar no formato do banco o campo start_event

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -45,4 +45,10 @@ class Event extends Model
         $this->attributes['slug'] = Str::slug($value);
 
     }
+
+    public function setStartEventAttribute($value){
+
+        $this->attributes['start_event'] = (\DateTime::createFromFormat('d/m/Y H:i', $value))->format('Y-m-d H:i');
+
+    }
 }
