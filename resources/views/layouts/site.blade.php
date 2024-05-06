cffb0420f084c9717b6e38e7fdf604cee7542f5b
Guilherme Lima <guilima102013@gmail.com>
2024-05-06 12:04:05 -0300

    Colocando menu na tela home

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -11,8 +11,49 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-5">
+
+      <a class="navbar-brand" href={{route('home')}}>Eventos</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav mr-auto">
+
+          <li class="nav-item active">
+            <a class="nav-link" href={{route('home')}}> Home <span class="sr-only">(current)</span></a>
+          </li>        
+
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              Categorias
+            </a>
+
+            <div class="dropdown-menu">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+
+        </ul>
+
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Nome evento" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+        </form>
+
+      </div>
+
+    </nav>
+
     <div class="container">
         @yield('content')
     </div>
+
 </body>
 </html>
