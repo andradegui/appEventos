b745a8650deccc9cd3bdfa12200260c16506cd39
Guilherme Lima <guilima102013@gmail.com>
2024-05-06 12:04:29 -0300

    Paginando eventos na tela home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,19 +7,25 @@ use App\Models\Event;
 
 class HomeController extends Controller
 {
+    public function __construct(Event $event){
+
+        $this->event = $event;
+
+    }
+
     public function index(){
 
-        $events = Event::all();
+        $events = $this->event->orderBy('start_event', 'DESC')->paginate(10);
         // $events = [];
 
         // return view('welcome', ['events' => $events]);
-        return view('welcome', compact('events'));
+        return view('home', compact('events'));
 
     }
 
     public function show($slug){
         
-        $event = Event::whereSlug($slug)->first();
+        $event = $this->whereSlug($slug)->first();
         return view('event', compact('event'));
 
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,72 @@
-@extends('layouts.app')
+@extends('layouts.site')
+
+@section('title')
+
+    Principais Eventos
+
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="row">
 
-                    {{ __('You are logged in!') }}
+        <div class="col-12">
+
+            <h3 class="text-center">Eventos</h3>
+
+        </div>
+
+    </div>
+
+    <hr>
+
+    <div class="row mb-4">
+    
+        @forelse( $events as $event )
+
+            <div class="col-4">
+
+                <div class="card">
+
+                    <img src="https://via.placeholder.com/640x480.png/20co50?text=Sem%20Imagem" alt="" class="card-img-top">
+
+                    <div class="card-body">
+
+                        <h5 class="card-title">{{ $event->title }}</h5>
+    
+                        <strong>Acontece em: {{ $event->start_event->format('d/m/Y H:i:s') }}</strong>
+    
+                        <p class="card-text">{{ $event->description }}</p>
+
+                       <p>Evento organizado por: <b>{{$event->owner_name}}</b></p>
+
+                        <a href="{{route('event.single', ['slug' => $event->slug])}}" class="btn btn-dark">Ver Evento</a>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            {{-- Fazendo tratamento que p/ cada 3 eventos seja criado uma nova div p/ agrupamento --}}
+            @if( ($loop->iteration % 3) == 0 ) <div class="row mb-4"></div> @endif 
+
+        @empty
+
+            <div class="col-12">
+                <div class="alert alert-warning">
+                    Nenhum evento encontrado...
                 </div>
             </div>
+
+        @endforelse
+
+    </div>
+
+    <div class="row">
+        <div class="col-12">
+            {{$events->links()}}
         </div>
     </div>
-</div>
+
 @endsection
