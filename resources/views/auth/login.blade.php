014774f797edee7c057c2c87e0c934446855efa2
Guilherme Lima <guilima102013@gmail.com>
2024-05-08 11:53:25 -0300

    Renomeando title das telas login e register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.auth')
 
+@section ('title') Login @endsection
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center" style="margin-top: 10%">
