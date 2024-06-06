be8df47e81b682bb29ef9a17648ad479a9990d6e
Guilherme Lima <guilima102013@gmail.com>
2024-06-06 14:41:05 -0300

    Ajuste no painel de ADM.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -127,10 +127,20 @@
 <body>
 <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
     <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Eventos</a>
+    
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <ul class="navbar-nav px-3">
+
+    <p class="h5 text-light">Olá {{auth()->user()->name}}</p>
+    
+    <ul class="navbar-nav px-3 flex-row">
+        
+
+        <li class="nav-item mr-3">
+            <a href="#" class="nav-link">Meu Perfil</a>
+        </li>
+
         <li class="nav-item text-nowrap">
             <a class="nav-link" href="{{route('index')}}" onclick="event.preventDefault(); document.getElementById('logout').submit()">Sair</a>
 
